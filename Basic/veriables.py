a2883b41cab7b43f94c11c24a11bd41133499665
WazedKhan <wazedkhan119399@gmail.com>
2023-01-07 21:40:51 +0600

    ver

---
+++ Basic/veriables.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+address = "loc" + " "
+
+earth = address+ "Earth: "
+mars = address + "Mars: "
+
+print(earth, "Hello")
+print(mars, "Hi")
+
+sender_name = "Khadiza"
+
+print(f"{earth} This message was sent from Planet Earth by {sender_name} at {50+10}")
